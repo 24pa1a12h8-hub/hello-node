4bb998f2f9a3fd20297cb2dea195214b5017c1a9
SHARMILA PEDIREDDY <24p1a12h8@vishnu.edu.in>
2026-09-21 20:46:05 +0530

    Complete HTTP server and registration route

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-function hello() {
-    console.log("Hello Node.Js! Trying it for the first time");
-}
+const http = require("http");
+const fs = require("fs");
 
-hello();
+const args = process.argv.slice(2);
+
+let port = 3000;
+
+args.forEach((arg) => {
+  if (arg.startsWith("--port=")) {
+    port = Number(arg.split("=")[1]);
+  }
+});
+
+const server = http.createServer((req, res) => {
+  let filename;
+
+  if (req.url === "/") {
+    filename = "home.html";
+  } else if (req.url === "/home") {
+    filename = "home.html";
+  } else if (req.url === "/project") {
+    filename = "project.html";
+  } else if (req.url === "/registration") {
+    filename = "registration.html";
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Page not found");
+    return;
+  }
+
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error loading page");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
